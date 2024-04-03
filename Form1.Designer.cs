9c8773d249fd0591ec3e95c369cf5de2bfdde7bb
oli4ka2009 <gnatetskaolia@gmail.com>
2024-04-03 12:47:31 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace LAB3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.plasmaButton = new System.Windows.Forms.Button();
+            this.algebraicButton = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.roughnessBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.scaleTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.iterationsTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.xOffsetTextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.yOffsetTextBox = new System.Windows.Forms.TextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(758, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Fractals";
+            // 
+            // plasmaButton
+            // 
+            this.plasmaButton.BackColor = System.Drawing.Color.Tan;
+            this.plasmaButton.Font = new System.Drawing.Font("Microsoft YaHei", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.plasmaButton.Location = new System.Drawing.Point(665, 43);
+            this.plasmaButton.Name = "plasmaButton";
+            this.plasmaButton.Size = new System.Drawing.Size(157, 55);
+            this.plasmaButton.TabIndex = 2;
+            this.plasmaButton.Text = "Plasma";
+            this.plasmaButton.UseVisualStyleBackColor = false;
+            this.plasmaButton.Click += new System.EventHandler(this.plasmaButton_Click);
+            // 
+            // algebraicButton
+            // 
+            this.algebraicButton.BackColor = System.Drawing.Color.Tan;
+            this.algebraicButton.Font = new System.Drawing.Font("Microsoft YaHei", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.algebraicButton.Location = new System.Drawing.Point(665, 204);
+            this.algebraicButton.Name = "algebraicButton";
+            this.algebraicButton.Size = new System.Drawing.Size(155, 55);
+            this.algebraicButton.TabIndex = 3;
+            this.algebraicButton.Text = "z*sin (z)+с";
+            this.algebraicButton.UseVisualStyleBackColor = false;
+            this.algebraicButton.Click += new System.EventHandler(this.algebraicButton_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(647, 623);
+            this.pictureBox.TabIndex = 4;
+            this.pictureBox.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(665, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Roughness";
+            // 
+            // roughnessBox
+            // 
+            this.roughnessBox.Location = new System.Drawing.Point(669, 124);
+            this.roughnessBox.Name = "roughnessBox";
+            this.roughnessBox.Size = new System.Drawing.Size(87, 26);
+            this.roughnessBox.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(665, 262);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Scale";
+            // 
+            // scaleTextBox
+            // 
+            this.scaleTextBox.Location = new System.Drawing.Point(669, 285);
+            this.scaleTextBox.Name = "scaleTextBox";
+            this.scaleTextBox.Size = new System.Drawing.Size(87, 26);
+            this.scaleTextBox.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(665, 314);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Iterations";
+            // 
+            // iterationsTextBox
+            // 
+            this.iterationsTextBox.Location = new System.Drawing.Point(669, 337);
+            this.iterationsTextBox.Name = "iterationsTextBox";
+            this.iterationsTextBox.Size = new System.Drawing.Size(87, 26);
+            this.iterationsTextBox.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(665, 366);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "X offset";
+            // 
+            // xOffsetTextBox
+            // 
+            this.xOffsetTextBox.Location = new System.Drawing.Point(669, 389);
+            this.xOffsetTextBox.Name = "xOffsetTextBox";
+            this.xOffsetTextBox.Size = new System.Drawing.Size(87, 26);
+            this.xOffsetTextBox.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(798, 366);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 20);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Y offset";
+            // 
+            // yOffsetTextBox
+            // 
+            this.yOffsetTextBox.Location = new System.Drawing.Point(802, 389);
+            this.yOffsetTextBox.Name = "yOffsetTextBox";
+            this.yOffsetTextBox.Size = new System.Drawing.Size(87, 26);
+            this.yOffsetTextBox.TabIndex = 14;
+            // 
+            // saveButton
+            // 
+            this.saveButton.BackColor = System.Drawing.Color.Tan;
+            this.saveButton.Font = new System.Drawing.Font("Microsoft YaHei", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.saveButton.Location = new System.Drawing.Point(665, 473);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(155, 55);
+            this.saveButton.TabIndex = 15;
+            this.saveButton.Text = "Save image";
+            this.saveButton.UseVisualStyleBackColor = false;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PapayaWhip;
+            this.ClientSize = new System.Drawing.Size(929, 647);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.yOffsetTextBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.xOffsetTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.iterationsTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.scaleTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.roughnessBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.algebraicButton);
+            this.Controls.Add(this.plasmaButton);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button plasmaButton;
+        private System.Windows.Forms.Button algebraicButton;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox roughnessBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox scaleTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox iterationsTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox xOffsetTextBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox yOffsetTextBox;
+        private System.Windows.Forms.Button saveButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+using System.Drawing.Imaging;
+using System.Globalization;
+
+namespace LAB3
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap fractalImage;
+        public double gRoughness;
+        public double gBigSize;
+        Random rnd = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void plasmaButton_Click(object sender, EventArgs e)
+        {
+            InitializePlasmaFractal();
+        }
+
+        private void InitializePlasmaFractal()
+        {
+            int width = pictureBox.Width;
+            int height = pictureBox.Height;
+
+            Color[] colors = new Color[4];
+
+            ColorDialog colorDialog = new ColorDialog();
+            for (int i = 0; i < 4; ++i)
+            {
+                if (colorDialog.ShowDialog() == DialogResult.OK)
+                {
+                    colors[i] = colorDialog.Color;
+                }
+            }
+
+            double[] reds = new double[4];
+            double[] greens = new double[4];
+            double[] blues = new double[4];
+
+            for (int i = 0; i < 4; ++i)
+            {
+                reds[i] = colors[i].R;
+                greens[i] = colors[i].G;
+                blues[i] = colors[i].B;
+            }
+
+            if (double.TryParse(roughnessBox.Text, NumberStyles.Float, CultureInfo.InvariantCulture, out double roughness))
+            {
+                if (roughness <= 1 && roughness >= 0)
+                {
+                    double[,] pointsR = Generate(width, height, roughness, reds);
+                    double[,] pointsG = Generate(width, height, roughness, greens);
+                    double[,] pointsB = Generate(width, height, roughness, blues);
+                    DrawFractal(pointsR, pointsG, pointsB);
+                }
+                else {
+                    MessageBox.Show("Roughness must be between 0 and 1");
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Roughness must be numerical value");
+                return;
+            }
+        }
+
+        public double[,] Generate(int iWidth, int iHeight, double iRoughness, double[] numbers)
+        {
+            double c1, c2, c3, c4;
+            double[,] points = new double[iWidth + 1, iHeight + 1];
+
+            c1 = numbers[0] / 255;
+            c2 = numbers[1] / 255;
+            c3 = numbers[2] / 255;
+            c4 = numbers[3] / 255;
+            gRoughness = iRoughness;
+            gBigSize = iWidth + iHeight;
+            DivideGrid(ref points, 0, 0, iWidth, iHeight, c1, c2, c3, c4);
+            return points;
+        }
+
+        public void DivideGrid(ref double[,] points, double x, double y, double width, double height, double c1, double c2, double c3, double c4)
+        {
+            double Edge1, Edge2, Edge3, Edge4, Middle;
+
+            double newWidth = Math.Floor(width / 2);
+            double newHeight = Math.Floor(height / 2);
+
+            if (width > 1 || height > 1)
+            {
+                Middle = ((c1 + c2 + c3 + c4) / 4) + Displace(newWidth + newHeight); 
+                Edge1 = ((c1 + c2) / 2);
+                Edge2 = ((c2 + c3) / 2);
+                Edge3 = ((c3 + c4) / 2);
+                Edge4 = ((c4 + c1) / 2);
+                                       
+                Middle = Rectify(Middle);
+                Edge1 = Rectify(Edge1);
+                Edge2 = Rectify(Edge2);
+                Edge3 = Rectify(Edge3);
+                Edge4 = Rectify(Edge4);
+            
+                DivideGrid(ref points, x, y, newWidth, newHeight, c1, Edge1, Middle, Edge4);
+                DivideGrid(ref points, x + newWidth, y, width - newWidth, newHeight, Edge1, c2, Edge2, Middle);
+                DivideGrid(ref points, x + newWidth, y + newHeight, width - newWidth, height - newHeight, Middle, Edge2, c3, Edge3);
+                DivideGrid(ref points, x, y + newHeight, newWidth, height - newHeight, Edge4, Middle, Edge3, c4);
+            }
+            else     
+            {
+                double c = (c1 + c2 + c3 + c4) / 4;
+
+                points[(int)(x), (int)(y)] = c;
+                if (width == 2)
+                {
+                    points[(int)(x + 1), (int)(y)] = c;
+                }
+                if (height == 2)
+                {
+                    points[(int)(x), (int)(y + 1)] = c;
+                }
+                if ((width == 2) && (height == 2))
+                {
+                    points[(int)(x + 1), (int)(y + 1)] = c;
+                }
+            }
+        }
+
+        private double Rectify(double iNum)
+        {
+            if (iNum < 0)
+            {
+                iNum = 0;
+            }
+            else if (iNum > 1.0)
+            {
+                iNum = 1.0;
+            }
+            return iNum;
+        }
+
+        private double Displace(double SmallSize)
+        {
+
+            double Max = SmallSize / gBigSize * gRoughness;
+            return (rnd.NextDouble() - 0.5) * Max;
+        }
+
+        private void DrawFractal(double[,] pointsR, double[,] pointsG, double[,] pointsB)
+        {
+            int width = pointsR.GetLength(0);
+            int height = pointsR.GetLength(1);
+
+            fractalImage = new Bitmap(width, height);
+
+            double minR = double.MaxValue;
+            double maxR = double.MinValue;
+            double minG = double.MaxValue;
+            double maxG = double.MinValue;
+            double minB = double.MaxValue;
+            double maxB = double.MinValue;
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    if (pointsR[x, y] < minR)
+                        minR = pointsR[x, y];
+                    if (pointsR[x, y] > maxR)
+                        maxR = pointsR[x, y];
+                    if (pointsB[x, y] < minB)
+                        minB = pointsB[x, y];
+                    if (pointsB[x, y] > maxB)
+                        maxB = pointsB[x, y];
+                    if (pointsG[x, y] < minG)
+                        minG = pointsG[x, y];
+                    if (pointsG[x, y] > maxG)
+                        maxG = pointsG[x, y];
+                }
+            }
+
+            // Draw fractal
+            using (Graphics g = Graphics.FromImage(fractalImage))
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    for (int x = 0; x < width; x++)
+                    {
+                        int colorValueR = (int)((pointsR[x, y] - minR) / (maxR - minR) * 255);
+                        int colorValueG = (int)((pointsG[x, y] - minG) / (maxG - minG) * 255);
+                        int colorValueB = (int)((pointsB[x, y] - minB) / (maxB - minB) * 255);
+                        Color color = Color.FromArgb(colorValueR, colorValueG, colorValueB);
+                        SolidBrush brush = new SolidBrush(color);
+                        g.FillRectangle(brush, x, y, 1, 1);
+                    }
+                }
+            }
+
+            pictureBox.Image = fractalImage;
+        }
+
+        public static Func<Complex, Complex, Complex> Fractal() => (c, z) => z * Complex.Sin(z) + c;
+
+        private void DrawAlgebraicFractal(double Scale, int Iterations, double xOffset, double yOffset)
+        {
+            for (int x = 0; x < pictureBox.Width; x++)
+            {
+                for (int y = 0; y < pictureBox.Height; y++)
+                {
+                    double a = (x - pictureBox.Width / 2.0) / Scale + xOffset;
+                    double b = (y - pictureBox.Height / 2.0) / Scale + yOffset;
+                    Complex c = new Complex(a, b);
+                    Complex z = Complex.Zero;
+                    int iterations = 0;
+
+                    while (iterations < Iterations)
+                    {
+                        z = Fractal()(c, z);
+                        iterations++;
+
+                        if (z.Magnitude >= 2)
+                            break;
+                    }
+
+                    int colorValue = (int)((double)iterations / Iterations * 255);
+                    Color color = Color.FromArgb(34, colorValue, colorValue);
+                    fractalImage.SetPixel(x, y, color);
+                }
+            }
+        }
+
+        private void algebraicButton_Click(object sender, EventArgs e)
+        {
+            fractalImage = new Bitmap(pictureBox.Width, pictureBox.Height);
+
+            if (!double.TryParse(scaleTextBox.Text, NumberStyles.Float, CultureInfo.InvariantCulture, out double scale))
+            {
+                MessageBox.Show("Incorrect scale!");
+                return;
+            }
+            if (!int.TryParse(iterationsTextBox.Text, out int iterations))
+            {
+                MessageBox.Show("Incorrect iterations number!");
+                return;
+            }
+            if (!double.TryParse(xOffsetTextBox.Text, NumberStyles.Float, CultureInfo.InvariantCulture, out double xOffset))
+            {
+                MessageBox.Show("Incorrect x offset!");
+                return;
+            }
+            if (!double.TryParse(yOffsetTextBox.Text, NumberStyles.Float, CultureInfo.InvariantCulture, out double yOffset))
+            {
+                MessageBox.Show("Incorrect y offset!");
+                return;
+            }
+
+            DrawAlgebraicFractal(scale, iterations, xOffset, yOffset);
+            pictureBox.Image = fractalImage;
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Зображення (*.png)|*.png|Всі файли (*.*)|*.*";
+            saveFileDialog1.Title = "Зберегти зображення";
+            saveFileDialog1.ShowDialog();
+
+            if (saveFileDialog1.FileName != "")
+            {
+                fractalImage.Save(saveFileDialog1.FileName, ImageFormat.Png);
+            }
+        }
+    }
+}
